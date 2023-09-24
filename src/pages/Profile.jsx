3805cd919ba6cd4a7fc809450f58d855655d3a74
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2023-09-24 21:09:36 +0530

    Changes to be committed: 	modified:   src/pages/Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,9 @@
 import User from "../components/User";
+import Header from "../components/Header";
 function Profile() {
     return (
    <div>
+    {/* <Header/> */}
      <User/>  
   
       </div>
